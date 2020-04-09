13157955f7e8634dd464b9105d3dafee90225709
biskoi <biskoi@daydream.cafe>
2020-04-09 02:34:30 -0600

    project mvp

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code away!
+const server = require('./server');
+const port = 4500;
+
+server.listen(port, () => {
+   console.log(`Server running on port ${port}`)
+})
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 
+const userRouter = require('./users/userRouter');
 const server = express();
+
+server.use(express.json())
+server.use(logger)
+server.use('/api/users', userRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +13,9 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+console.dir(`${req.method} to ${req.originalUrl} with query ${req.query} at ${new Date().toISOString()}`)
+next();
+}
 
 module.exports = server;
